5aef01aa61e489a25da388f8233f84541068b69f
Elly Maimon <eliranmaimon@gmail.com>
2018-08-17 11:20:03 -0700

    add television component

---
+++ src/app/television/television.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-television',
+  templateUrl: './television.component.html',
+  styleUrls: ['./television.component.css']
+})
+export class TelevisionComponent {
+
+  constructor() { }
+
+}
