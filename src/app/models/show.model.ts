f19b34986a12ad3daec6cfccd067afdbf582f3a9
Elly Maimon <eliranmaimon@gmail.com>
2018-08-17 13:48:40 -0700

    refactor show model

--- src/app/models/show.model.ts
+++ src/app/models/show.model.ts
@@ -1,6 +1,12 @@
 import { Season } from './season.model';
 
 export class Show {
-  constructor(public id: number, public seasons: Season[], public title: string, public genre: string,
-              public network: string, public synopsis: string){}
+  constructor(
+    public id: number,
+    public seasons: Season[],
+    public title: string,
+    public genre: string,
+    public network: string,
+    public synopsis: string
+  ){}
 }
