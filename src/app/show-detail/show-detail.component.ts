116eb3b22d5aa61f9a9939f6efbd9d57636f7995
Elly Maimon <eliranmaimon@gmail.com>
2018-08-17 13:29:24 -0700

    add lifecycle hook methods to show detail component, import show

--- src/app/show-detail/show-detail.component.ts
+++ src/app/show-detail/show-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Show } from '../models/show.model';
 
 @Component({
   selector: 'app-show-detail',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-detail.component.css']
 })
 export class ShowDetailComponent implements OnInit {
+  showId: number = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.showId = parseInt(urlParameters['id']);
+    });      
   }
 
 }
