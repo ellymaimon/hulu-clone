bc1a1cbda9112ae43289314cea4059083747587b
Elly Maimon <eliranmaimon@gmail.com>
2018-08-17 13:49:07 -0700

    add service and provider to show-detail component

--- src/app/show-detail/show-detail.component.ts
+++ src/app/show-detail/show-detail.component.ts
@@ -2,16 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Show } from '../models/show.model';
+import { ShowService } from '../show.service';
 
 @Component({
   selector: 'app-show-detail',
   templateUrl: './show-detail.component.html',
-  styleUrls: ['./show-detail.component.css']
+  styleUrls: ['./show-detail.component.css'],
+  providers: [ShowService]
 })
+
 export class ShowDetailComponent implements OnInit {
   showId: number = null;
 
-  constructor(private route: ActivatedRoute, private location: Location) {}
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private showService: ShowService
+  ) {}
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
