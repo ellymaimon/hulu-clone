e77b1dce3b873d4a07e3bac892ae1c1ebd59a7d5
Elly Maimon <eliranmaimon@gmail.com>
2018-08-17 13:48:30 -0700

    refactor movie model

--- src/app/models/movie.model.ts
+++ src/app/models/movie.model.ts
@@ -1,4 +1,13 @@
 export class Movie {
-  constructor(public id: number, public title: string, public genre: string, public director: string, public actors: string[],
-              public synopsis: string, public year: number, public runtime: number, public rating: string) { }
+  constructor(
+    public id: number,
+    public title: string,
+    public genre: string,
+    public director: string,
+    public actors: string[],
+    public synopsis: string,
+    public year: number,
+    public runtime: number,
+    public rating: string
+  ) {}
 }
