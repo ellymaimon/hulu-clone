6e799ee555b817633cb997a0a09416275f16b489
Elly Maimon <ellymaimon@Ellys-MacBook-Pro-2.local>
2018-08-21 11:38:16 -0700

    add firebase information to show service (need to fix by id method)

--- src/app/show.service.ts
+++ src/app/show.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Show } from './models/show.model';
 import { SHOWS } from './mock-shows';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class ShowService {
+  shows: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.shows = database.list('shows');
+  }
 
   getShows() {
-    return SHOWS;
+    return this.shows;
   }
 
   getShowById(showId: number){
-    for (var i = 0; i <= SHOWS.length - 1; i++) {
-      if (SHOWS[i].id === showId) {
-        return SHOWS[i];
-      }
-    }
+    // for (var i = 0; i <= SHOWS.length - 1; i++) {
+    //   if (SHOWS[i].id === showId) {
+    //     return SHOWS[i];
+    //   }
+    // }
   }
 }
