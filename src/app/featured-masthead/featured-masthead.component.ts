74e144643f5123a2592b935f83ce464ea45263e7
Elly Maimon <ellymaimon@Ellys-MacBook-Pro-2.local>
2018-08-24 12:49:14 -0700

    add featured masthead component

---
+++ src/app/featured-masthead/featured-masthead.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-masthead',
+  templateUrl: './featured-masthead.component.html',
+  styleUrls: ['./featured-masthead.component.css']
+})
+export class FeaturedMastheadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
